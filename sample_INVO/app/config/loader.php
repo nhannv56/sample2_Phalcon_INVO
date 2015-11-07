d7e782c29145c9c810ae0efad373fe675fd8992f
Nhan <ngo.nhan.09@gmail.com>
2015-11-07 17:25:18 +0700

    [invo] add autoload libraryDir to load elements

--- sample_INVO/app/config/loader.php
+++ sample_INVO/app/config/loader.php
@@ -7,5 +7,6 @@ $loader = new \Phalcon\Loader ();
 $loader->registerDirs ( array (
 		$config->application->controllersDir,
 		$config->application->modelsDir,
-		$config->application->pluginsDir 
+		$config->application->pluginsDir,
+		$config->application->libraryDir
 ) )->register ();
